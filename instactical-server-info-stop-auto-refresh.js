bb97a6a794b4cb6a1944e59304d94f9096d46c33
James <heyjames@users.noreply.github.com>
2022-02-01 15:56:13 -0800

    Create instactical-server-info-stop-auto-refresh.js

---
+++ instactical-server-info-stop-auto-refresh.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Stop auto-refresh info.instactical.com
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      /^http:\/\/info.instactical.com$/
+// @icon         https://www.google.com/s2/favicons?domain=instactical.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    window.stop();
+})();
