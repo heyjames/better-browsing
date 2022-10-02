3b3ca48b1d02fee000cd90fee63e24e2ecf378ea
James <heyjames@users.noreply.github.com>
2022-10-02 00:14:21 -0700

    Create tampermonkey-glassdoor-scroll.js

---
+++ tampermonkey-glassdoor-scroll.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Glassdoor Scroll
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.glassdoor.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=glassdoor.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.onscroll = null;
+})();
