752e31adbe7f7d5b69e4a6676936f7ba1c5bdc11
James <heyjames@users.noreply.github.com>
2023-01-19 19:55:27 -0800

    Update tampermonkey-twitch-disable-video-change.js

--- tampermonkey-twitch-disable-video-change.js
+++ tampermonkey-twitch-disable-video-change.js
@@ -1,15 +1,29 @@
 // ==UserScript==
-// @name         Disable Auto Background Video Quality Change
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       Slapsy
-// @match        https://player.twitch.tv/*
+// @name         Twitch - Disable automatic video downscale
+// @namespace    CommanderRoot
+// @copyright    CommanderRoot
+// @version      1.0.3
+// @description  Disables the automatic downscaling of Twitch streams while tabbed away
+// @author       https://twitter.com/CommanderRoot
 // @match        https://www.twitch.tv/*
+// @match        https://player.twitch.tv/*
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
+"use strict";
 
-(function() {
-    'use strict';
-    Object.defineProperty(document, "hidden", { value : false});
-})();
+// Try to trick the site into thinking it's never hidden
+Object.defineProperty(document, 'hidden', {value: false, writable: false});
+Object.defineProperty(document, 'visibilityState', {value: 'visible', writable: false});
+Object.defineProperty(document, 'webkitVisibilityState', {value: 'visible', writable: false});
+document.dispatchEvent(new Event('visibilitychange'));
+document.hasFocus = function () { return true; };
+
+// visibilitychange events are captured and stopped
+document.addEventListener('visibilitychange', function(e) {
+    e.stopImmediatePropagation();
+}, true, true);
+
+// Set the player quality to "Source"
+window.localStorage.setItem('s-qs-ts', Math.floor(Date.now()));
+window.localStorage.setItem('video-quality', '{"default":"chunked"}');
